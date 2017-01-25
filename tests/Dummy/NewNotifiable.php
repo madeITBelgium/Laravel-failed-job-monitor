334e70f4da9f6efce7e555d72bceb60e26c00896
madeITBelgium <info@madeit.be>
2017-01-25 13:58:10 +0000

    Apply fixes from StyleCI

--- tests/Dummy/NewNotifiable.php
+++ tests/Dummy/NewNotifiable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MadeITBelgium\LaravelFailedJobMonitor\Test\Dummy;
 
 use Illuminate\Notifications\Notifiable as NotifiableTrait;
@@ -6,16 +7,17 @@ use Illuminate\Notifications\Notifiable as NotifiableTrait;
 class NewNotifiable
 {
     use NotifiableTrait;
+
     public function routeNotificationForMail()
     {
         return 'info@example.com';
     }
-    
+
     public function routeNotificationForSlack()
     {
         return '';
     }
-    
+
     public function getKey()
     {
         return 1;
